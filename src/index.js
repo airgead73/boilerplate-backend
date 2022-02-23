a7fea58e2821f4eff791bb3265de27c6d26311f4
Dan Moneypenny <catullus1973@gmail.com>
2022-02-23 04:39:26 -0800

    update

--- src/index.js
+++ src/index.js
@@ -20,28 +20,22 @@ const apiRouter = express.Router();
  *  App Configuration
  */
 
- var whitelist = ['https://dpm-sandbox-api.herokuapp.com']
- var corsOptionsDelegate = function (req, callback) {
-   var corsOptions;
-   if (whitelist.indexOf(req.header('Origin')) !== -1) {
-     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-   } else {
-     corsOptions = { origin: false } // disable CORS for this request
-   }
-   callback(null, corsOptions) // callback expects two parameters: error and options
- }
+ app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://dpmsandbox.dev/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(helmet());
-app.use(cors(corsOptionsDelegate));
+app.use(cors({ origin: clientOrigins }));
 app.use(express.json());
 
 app.use("/api", apiRouter);
 
 apiRouter.use("/messages", messagesRouter);
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  })
 
 app.use(function (err, req, res, next) {
   console.log(err);
