87fed665b8fb3f5a7511760e1b28d1588d1da5ea
Dan Moneypenny <catullus1973@gmail.com>
2022-02-23 04:23:02 -0800

    update

--- src/messages/messages.service.js
+++ src/messages/messages.service.js
@@ -4,7 +4,7 @@
 
 const getPublicMessage = () => {
   return {
-    message: "The API doesn't require an access token to share this message [fix clientorigins].",
+    message: "The API doesn't require an access token to share this message [fix cors].",
   };
 };
 
