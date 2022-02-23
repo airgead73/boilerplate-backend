7ec4aecf9488c10faf5ff957232d3baa23682774
Dan Moneypenny <catullus1973@gmail.com>
2022-02-23 04:21:28 -0800

    update cors

--- src/index.js
+++ src/index.js
@@ -20,8 +20,19 @@ const apiRouter = express.Router();
  *  App Configuration
  */
 
+ var whitelist = ['https://dpm-sandbox-api.herokuapp.com']
+ var corsOptionsDelegate = function (req, callback) {
+   var corsOptions;
+   if (whitelist.indexOf(req.header('Origin')) !== -1) {
+     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+   } else {
+     corsOptions = { origin: false } // disable CORS for this request
+   }
+   callback(null, corsOptions) // callback expects two parameters: error and options
+ }
+
 app.use(helmet());
-app.use(cors({ origin: clientOrigins }));
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api", apiRouter);
